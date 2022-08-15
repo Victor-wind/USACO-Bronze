79154c46f5066f282c4404dfc39d3f232ab15f58
bluewater <70036495+Victor-wind@users.noreply.github.com>
2022-08-14 21:38:14 -0500

    Update cow_gymnastics.py

--- solution/dec_2019/cow_gymnastics.py
+++ solution/dec_2019/cow_gymnastics.py
@@ -43,3 +43,39 @@ for k,v in pos_dic.items():
 with open('gymnastics.out', 'w') as f:
     f.write(str(cnt))
 
+'''
+f = open('gymnastics.in')
+lines = f.readlines()
+lines[0] =lines[0].split()
+K = int(lines[0][0])
+N = int(lines[0][1])
+nums_input = [i.split() for i in lines[1:]]
+nums = list()
+for x in nums_input:
+    y = [int(i) for i in x]
+    nums.append(y)
+
+cnt = 0
+def check_order(sessions, k, i, j):
+    pairs = 0
+    for session in sessions:
+        for x in range(len(session)):
+            if session[x]==i:
+                i_index=x
+            if session[x]==j:
+                j_index=x
+        if i_index < j_index:
+            pairs += 1
+    return pairs
+
+for i in range(1, N+1):
+    for j in range(1, N+1):        
+        pairs = check_order(nums, K, i, j)
+        if pairs==K:
+            cnt+=1
+
+f = open('gymnastics.out','w')
+f.write(str(cnt))
+'''
+
+# contributed by lazy panda.
