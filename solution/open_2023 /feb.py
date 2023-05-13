6238ebe6d0d1b66d90bb2798c7f7e35d55e3699b
bluewater <70036495+Victor-wind@users.noreply.github.com>
2023-05-13 16:23:10 -0500

    Update feb.py

--- solution/open_2023 /feb.py
+++ solution/open_2023 /feb.py
@@ -54,3 +54,77 @@ print(len(answer))
 for i in answer:
     print(basic_cnt+i)
     
+'''
+N = int(input())
+msg = input().rstrip()
+#print(N)
+#print(msg)
+
+# The solution is based on dynamic programing
+# range_prev_B is the possible min and max distinct excitement levels if ith msg is B
+# range_prev_E is the possible min and max distinct excitement levels if ith msg is E
+# At the index i, if msg[i] is 'B' or 'E', then range_prev_E/range_prev_B is None.
+# range_prev_B[i] only depends on range_prev_B[i-1] and range_prev_E[i-1]
+# if msg[i] == 'B', range_prev_E[i] is None, range_prev_B[i] = range_prev_B[i-1]+1 union range_prev_E[i-1]
+# if msg[i] == 'F', range_prev_E and range_prev_B will Not be None
+# For example msg = 'BFFFB'
+#            range_prev_B   range_prev_E
+#     B       [0,0]          None [99999999999999999999999999, -1]
+#     F       [1,1]          [0,0]
+#     F       [0,2] (0,2)    [1,1]  
+#     F       [1,3] (1,3)    [0,2]  
+#     B       [0,4] (0,2,4)  None
+# the results will be range_prev_E[i-1] union range_prev_E[i-1]
+# to speed up the execution, observe the steps in results is 1 (msg[0] == 'F' || msg[-1] == 'F') or 2. 
+first_letter = msg[0]
+range_prev_B = [99999999999999999999999999, -1]
+range_prev_E = [99999999999999999999999999, -1]
+
+if msg[0] == 'F':
+    first_letter = 'BE'
+
+for x in first_letter:
+    if x == 'B':
+       range_prev_B = [0,0]
+    if x == 'E':
+       range_prev_E = [0,0]
+
+for i in range(1,len(msg)):
+    c = msg[i]
+    letters = list(c)
+    if c == 'F':
+        letters = list('BE')
+
+    range_cur_B = [99999999999999999999999999, -1]
+    range_cur_E = [99999999999999999999999999, -1]
+
+    for letter in letters:
+        mn, mx = 99999999999999999999999999, -1       
+        if letter == 'B':
+            mn = min(range_prev_B[0]+1, range_prev_E[0])
+            mx = max(range_prev_B[1]+1, range_prev_E[1])
+            range_cur_B[0] = mn 
+            range_cur_B[1] = mx
+        if letter == 'E':
+            mn = min(range_prev_E[0]+1, range_prev_B[0])
+            mx = max(range_prev_E[1]+1, range_prev_B[1])
+            range_cur_E[0] = mn 
+            range_cur_E[1] = mx
+        
+    range_prev_E[0] = range_cur_E[0]
+    range_prev_E[1] = range_cur_E[1]
+    range_prev_B[0] = range_cur_B[0]
+    range_prev_B[1] = range_cur_B[1]
+    #print(f'{c=} {range_prev_B=} {range_prev_E=}')    
+    
+
+mn_res = min(range_prev_E[0], range_prev_B[0]) 
+mx_res = max(range_prev_E[1], range_prev_B[1])
+
+step = 1 if msg[0] == 'F' or msg[-1] == 'F' else 2
+result = range(mn_res, mx_res+1, step)
+
+print(len(result))
+for v in result:
+    print(v)
+'''
