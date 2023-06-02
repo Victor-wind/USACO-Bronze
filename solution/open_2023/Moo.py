932e1e81ec1083c58891839c48fd06e2960ef449
bluewater <70036495+Victor-wind@users.noreply.github.com>
2023-06-02 15:21:48 -0500

    Update Moo.py

--- solution/open_2023/Moo.py
+++ solution/open_2023/Moo.py
@@ -15,15 +15,11 @@ def solve(commas: int, periods:int, words_type_list: dict) -> None:
     # Step 1: try to make more sentences: first type 1: noun+intransitives, then type 2 noun + transitive verb + noun(s)
     # the reason is type 1 sentences use more nouns. For example, 2 nouns can make A1, B2, but A1B.
     while len(words_type_list['noun']) > 0 and len(words_type_list['intransitive-verb']) > 0:
-        sentences_intransitives.append(words_type_list['noun'][-1] + ' ' + words_type_list['intransitive-verb'][-1])
-        words_type_list['noun'].pop()
-        words_type_list['intransitive-verb'].pop()
+        sentences_intransitives.append(words_type_list['noun'].pop() + ' ' + words_type_list['intransitive-verb'].pop())
+
     while len(words_type_list['noun']) > 1 and len(words_type_list['transitive-verb']) > 0:
-        sentences_transitives.append(words_type_list['noun'][-1] + ' ' + words_type_list['transitive-verb'][-1] +
-                                     ' ' + words_type_list['noun'][-2])
-        words_type_list['noun'].pop()
-        words_type_list['noun'].pop()
-        words_type_list['transitive-verb'].pop()
+        sentences_transitives.append(words_type_list['noun'].pop() + ' ' + words_type_list['transitive-verb'].pop() +
+                                     ' ' + words_type_list['noun'].pop())
 
     # The most possible sentences (sentences_possible) we can make is either (periods + conjunctions) if conjunctions <= periods;
     # or (periods + periods) if conjunctions > periods
@@ -35,49 +31,39 @@ def solve(commas: int, periods:int, words_type_list: dict) -> None:
     # the order is important. We want to keep type 2 sentences because one sentence uses 3 words. type 1 sentence uses 2 words.
     sentences_decompose = len(sentences_intransitives) + len(sentences_transitives) - sentences_possible
     while len(sentences_intransitives) > 0 and sentences_decompose > 0:
-        noun, v = sentences_intransitives[-1].split()
+        noun, v = sentences_intransitives.pop().split()
         words_type_list['noun'].append(noun)
         words_type_list['intransitive-verb'].append(v)
-        sentences_intransitives.pop()
         sentences_decompose -= 1
     # Step 2.B: decompose type 2 [noun + transitive + noun] sentences if necessary to make more nouns available.
     while len(sentences_transitives) > 0 and sentences_decompose > 0:
-        noun1, v, noun2 = sentences_transitives[-1].split()
+        noun1, v, noun2 = sentences_transitives.pop().split()
         words_type_list['noun'].append(noun1)
         words_type_list['noun'].append(noun2)
         words_type_list['transitive-verb'].append(v)
-        sentences_transitives.pop()
         sentences_decompose -= 1
 
     # Step 3.A replace type 1 sentences with type 2 sentences, if possible. so more nouns are used.
     # Step 3.A vs Step 3.B order does not matter.
     while len(words_type_list['noun']) > 0 and len(words_type_list['transitive-verb']) > 0 \
             and len(sentences_intransitives) > 0:
-        noun, v = sentences_intransitives[-1].split()
+        noun, v = sentences_intransitives.pop().split()
         words_type_list['intransitive-verb'].append(v)
-        sentences_intransitives.pop()
-        sentences_transitives.append(noun + ' ' + words_type_list['transitive-verb'][-1] + ' ' +
-                                     words_type_list['noun'][-1])
-        words_type_list['noun'].pop()
-        words_type_list['transitive-verb'].pop()
+        sentences_transitives.append(noun + ' ' + words_type_list['transitive-verb'].pop() + ' ' +
+                                     words_type_list['noun'].pop())
 
     # Step 3.B add ','+ noun to sentences
     while len(words_type_list['noun']) > 0 and commas > 0 and len(sentences_transitives) > 0:
-        sentence_commas = sentences_transitives[-1]
-        sentences_transitives.pop()
-        sentence_commas = sentence_commas + ', '+words_type_list['noun'][-1]
+        sentence_commas = sentences_transitives.pop()
+        sentence_commas = sentence_commas + ', '+words_type_list['noun'].pop()
         sentences_transitives.append(sentence_commas)
-        words_type_list['noun'].pop()
         commas -= 1
 
     # Step 4 compose sentences
     sentences_to_join = sentences_intransitives + sentences_transitives
     while len(words_type_list['conjunction']) > 0 and len(sentences_to_join) > 1:
-        sentences_compound.append(sentences_to_join[-1] + ' ' + words_type_list['conjunction'][-1]
-                                  + ' ' + sentences_to_join[-2]+'.')
-        words_type_list['conjunction'].pop()
-        sentences_to_join.pop()
-        sentences_to_join.pop()
+        sentences_compound.append(sentences_to_join.pop() + ' ' + words_type_list['conjunction'].pop()
+                                  + ' ' + sentences_to_join.pop()+'.')             
 
     # Step 5 join sentences
     sentences_compound_str = ''
