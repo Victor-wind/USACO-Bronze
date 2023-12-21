d93eb3ad843032a37083133e9e4dd496f03a96a0
bluewater <70036495+Victor-wind@users.noreply.github.com>
2023-12-21 11:13:44 -0600

    Create candy_cane.py

---
+++ solution/dec_2023/candy_cane.py
@@ -0,0 +1,16 @@
+N, M = [ int(x) for x in input().split()]
+heights_cow = [ int(x) for x in input().split()]
+heights_cane = [ int(x) for x in input().split()]
+
+#print(f' {heights_cow=} {heights_cane=}')
+for i in range(M):
+    cane_h, cane_l = heights_cane[i], 0    
+    for j in range(N):
+        if heights_cow[j] > cane_l:
+            eat = min(cane_h,heights_cow[j]) - cane_l
+            heights_cow[j] += eat
+            cane_l += eat
+
+for h in heights_cow:
+    print(h)
+        
