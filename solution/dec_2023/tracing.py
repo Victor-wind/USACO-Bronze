7d1414770f401b33a65b029681bead5237a6868f
bluewater <70036495+Victor-wind@users.noreply.github.com>
2023-12-25 12:09:10 -0600

    Update tracing.py

--- solution/dec_2023/tracing.py
+++ solution/dec_2023/tracing.py
@@ -1,64 +1,42 @@
-N = int(input())
-cows_str = input()
+import math
 
-cows = [0] * (N+2)
-for i in range(N):
-    cows[i+1] =  int(cows_str[i])
+N = int(input())
+cows = [int(x) for x in input()]
 
 #print(f"{N=} {cows=}")
-
-# to solve it in O(N) times, introduce 2 lists to store the boundaries of infected cows
-
-left_boundary = list()
-right_boundary = list()
-
-for i in range(1,N+1,1):
-    if cows[i] == 1 and cows[i-1] == 0:
-        left_boundary.append(i)
-    if cows[i] == 1 and cows[i+1] == 0:
-        right_boundary.append(i)
-        
-#print(f"{left_boundary=} {right_boundary=}")
-
-condi = True
-m, n = -1, -1
-while condi:
-    m, n = -1, -1
-    # from boundaries move towards
-    for i in range(len(left_boundary)):
-        if cows[left_boundary[i]] == 1 and cows[left_boundary[i]+1] == 1:
-            cows[left_boundary[i]] = 0
-            left_boundary[i] +=1
-        else:
-            m = i; condi = False
-            break
-
-    if not condi: break
-    m = len(left_boundary)
-
-    for j in range(len(right_boundary)):
-        if cows[right_boundary[j]] == 1 and cows[right_boundary[j]-1] == 1:
-            cows[right_boundary[j]] = 0
-            right_boundary[j] -= 1
-        else:
-            n = j; condi = False
-            break
-
-#print(f"{cows=}")
-#print(f"{m=} {n=}")
-
-# restore cows back
-for i in range(m-1, -1, -1):
-    boundary = left_boundary[i]-1
-    cows[boundary] = 1
-
-for i in range(n-1, -1, -1):
-    boundary = right_boundary[i]+1
-    cows[boundary] = 1
-
-#print(f"{cows[1:-1:1]=}")
+groups = list()
 cnt = 0
-for x in cows:
-    if x == 1: cnt += 1
+for i in cows:
+    if i == 1:
+        cnt+=1
+    else:
+        if cnt > 0:  groups.append(cnt)
+        cnt = 0
+if cnt > 0:  groups.append(cnt)
 
-print(cnt)            
+# calculate the min of max windows (days) each group of infected cows
+# for (2K+1) continual infected cows, the max days is K starting from 1 cow in the middle.
+# for 2K continual infected cows, the max days is K-1 starting from 2 cows in middle.
+# so the max days is (X-1)//2
+# if the K infected cows start from the beginning or end, the max days is K-1
+min_days = N
+for i,v in enumerate(groups):
+    days = (v-1)//2
+    if (i == 0 and cows[0] == 1) or (i == len(groups)-1 and cows[-1] == 1):
+        days = v-1
+    min_days = min(min_days,days)
+win = 1+2*min_days
+#print(f'{min_days=} {win=}')
+#print(groups)
+
+# for each group, how many infected cows are needed, so that after k days, the total # becomes groups[i]?
+# if starting with 1 middle infected cow,  after k days, the total is 1+2k
+# if starting with 2 middle infected cows, after k days, the total is 2+2k
+# to make the # initial cows small, always start with 1 middle infected cow.
+# need [groups[i]/(1+2k)] infected cow, so that after k days, the total # becomes groups[i]
+cnt = 0
+for x in groups:
+    y = math.ceil(x/win)
+    cnt += y
+    #print(y)
+print(cnt)
