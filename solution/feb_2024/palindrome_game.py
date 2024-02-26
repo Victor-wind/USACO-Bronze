94dc73de658beb3f46074e2e994cbd472546392f
bluewater <70036495+Victor-wind@users.noreply.github.com>
2024-02-26 10:45:26 -0600

    Update palindrome_game.py

--- solution/feb_2024/palindrome_game.py
+++ solution/feb_2024/palindrome_game.py
@@ -1,7 +1,13 @@
+'''
+1. if T is [1, 9], Bessie removes T stones; Elsie has no stones. Bessie wins.
+2. if T is 10, Bessie could not removes 10 stones as 10 is not palindromes number; No matter what K stones Bessie removes, Elsie removes the remaining 10-K stones. Elsie wins.
+3. if T is in [11, 19], Bessie removes T%10 stones, leaving 10 stones to Elsie; as in case 2, Elsie could not win.  Bessie wins.
+4. if T is 20, Bessie could not removes 10 or 20 stones as 10 / 20 is not palindromes number; No matter what K stones Bessie removes, Elsie takes 10-K%10 stones, leaving Bessie 10*N stones. Elsie wins.
+...
+this process continues, it concludes that anyone who has 10*N stones first loses, the other wins. 
+'''
 T = int(input())
-for i in range(T):
+for _ in range(T):
     s = int(input())
-    if s % 10 != 0:
-        print('B')
-    else:
-        print('E')    
+    print('B' if s % 10 else 'E')
+    
