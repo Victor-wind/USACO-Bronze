617409828eb9e88dab325633c76d6a9931c47440
bluewater <70036495+Victor-wind@users.noreply.github.com>
2025-09-04 21:16:18 -0500

    Update GuessAnimal_19_Jan.py

--- solution/jan_2019/GuessAnimal_19_Jan.py
+++ solution/jan_2019/GuessAnimal_19_Jan.py
@@ -7,21 +7,28 @@ animals = {}
 # process input data
 for i in range(N):
     t = lines[i+1].split()
-    animals[t[0]] = t[2::]
+    animals[t[0]] = set(t[2::])
 
-#print(animals)
+print(animals)
 
-max_yes = 1
-
+# for each animal, find the most 'yes' it could get
+# then select the animal with the most 'yes'
+most_yes_by_animal = list()
 for x in animals:
-    for y in animals:
+    cnt_x = 0
+    # for given animal x, it could have at most len(animals[x]) 'Yes'
+    # for its best solution, the feasible set contains all animals at beginning.
+    # with questions ongoing, the animals are removed from the feasible set gradually
+    # till only animal x and y are in the set
+    # the max 'Yes' is animals[x] & animals[y]
+    # for all possible animal y, find the best one together with the given animal x   
+    for y in animals:        
         if x == y: continue
-        z = set(animals[x]) & set(animals[y])
-        max_yes = max(max_yes, len(z)) 
+        z = animals[x] & animals[y]
+        cnt_x = max(cnt_x, len(z))
+    most_yes_by_animal.append(cnt_x) 
         
-#print(max_yes+1)
+result = (max(most_yes_by_animal)+1)
+print(result)
 with open('guess.out','w') as f:
-     f.write(str(max_yes+1))
-    
-
-
+     f.write(f'{result}')
