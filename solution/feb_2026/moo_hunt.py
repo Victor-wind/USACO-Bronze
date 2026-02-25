ec05bfa9972cd3a34528222fd18c626bdd514d6a
bluewater <70036495+Victor-wind@users.noreply.github.com>
2026-02-25 10:38:06 -0600

    Update moo_hunt.py

--- solution/feb_2026/moo_hunt.py
+++ solution/feb_2026/moo_hunt.py
@@ -17,25 +17,23 @@ scores = [ 0 for i in range(1<<N)]
 for mask in range(1,1<<N):
     # mask's bits map represents s board
     msb = mask.bit_length() - 1
-    # mask ^ (1 << msb) is a number removing the MSB bit
+    # mask ^ (1 << msb) -> removing the MSB bit from mask
     scores[mask] = scores[mask ^ (1 << msb)]
+    M_pos = [ i for i in range(msb) if (mask & 1<<i)]
+    O_pos = [ i for i in range(N) if not (mask & 1<<i) ]
+    
     # addtional scores by turning msb from 0/'O' to 1/'M'
-    for i in range(N):
-        if (mask & 1<<i): # if i == 1/'M', skip
-            continue
-        for j in range(i+1, N):
-            if not (mask & 1<<j):
-                scores[mask] += cells_pos[msb][i][j]
+    for i in O_pos:
+        for j in O_pos:
+            scores[mask] += cells_pos[msb][i][j]            
+
     # scores also could be reduced by turning msb from 0/'O' to 1/'M'
-    for m in range(msb):
-        if not (mask & 1<<m): # if m == 0/'O', skip
-            continue        
-        for i in range(msb-1, N):
-            if not (mask & 1<<i):
-                scores[mask] -= cells_pos[m][msb][i]
-        for j in range(msb):
-            if not (mask & 1<<j):
-                scores[mask] -= cells_pos[m][j][msb]
+    for m in M_pos:
+        for i in O_pos:
+            scores[mask] -= cells_pos[m][msb][i]
+        for j in O_pos:
+            scores[mask] -= cells_pos[m][j][msb]
+    
     
 best_score = max(scores)
 cnt = scores.count(best_score)
